8241c4d990fbf82065348bfa39452577da80bfb9
carlos oliveira <du.felipe.br@gmail.com>
2024-06-05 23:13:37 -0300

    apibronco config

--- Program.cs
+++ Program.cs
@@ -120,11 +120,11 @@ var app = builder.Build();
 app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
